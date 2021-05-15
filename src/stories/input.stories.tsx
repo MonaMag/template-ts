c0580e4b05170a0e4803200570977db0761041d0
Mona Mag <64916820+MonaMag@users.noreply.github.com>
2021-05-15 16:43:48 +0300

    L13 Controlled-input-checkbox-select

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -1,5 +1,6 @@
 import React, {ChangeEvent, useRef, useState} from 'react';
 import {Story, Meta} from '@storybook/react';
+import {action} from "@storybook/addon-actions";
 
 
 export default {
@@ -41,5 +42,38 @@ export const GetValueOfUncontrolledInputByButtonPress = () => {
     </>
 }
 
+
+
+export const ControlledInput = () => {
+    //заставляем компоненту перерисоваться, благодаря использованию локального стейта
+    const [parentValue, setParentValue] = useState('');
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setParentValue((e.currentTarget.value))
+    }
+    return <input value={parentValue} onChange={onChangeHandler}/>
+}
+
+export const ControlledCheckbox = () => {
+    const [parentValue, setParentValue] = useState(true);
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(e.currentTarget.checked)
+    }
+    return <input type='checkbox' checked={parentValue} onChange={onChangeHandler}/>
+};
+
+
+
+export const ControlledSelect = () => {
+    const [parentValue, setParentValue] = useState<string | undefined>(undefined);
+
+    const onChangeHandler = (e: ChangeEvent<HTMLSelectElement>) => {
+        setParentValue(e.currentTarget.value)
+    }
+    return <select value={parentValue} onChange={onChangeHandler}>
+        <option value={1}>Moscow</option>
+        <option value={2}>Minsk</option>
+        <option value={3}>Kiev</option>
+    </select>
+}
 
 

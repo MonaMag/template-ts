d0bd12b0aea8a4fd0c290184b6bb720ca05ca72c
Mona Mag <64916820+MonaMag@users.noreply.github.com>
2021-07-03 01:36:21 +0300

    L19 advanced-useState

---
+++ src/stories/UseState.stories.tsx
@@ -0,0 +1,27 @@
+import React, {useMemo, useState} from "react";
+import {Meta} from "@storybook/react";
+
+export default {
+    title: 'useState demo'
+    //component: ReactMemo
+} as Meta
+
+function generateData() {
+    //difficult counting
+    console.log('generateData')
+    return 1;
+}
+
+
+export const Example = () => {
+    console.log('Example')
+
+    //const initValue = useMemo(generateData, [])
+
+    const [counter, setCounter] = useState(generateData);
+
+    return <>
+        <button onClick={() => setCounter((state) => state + 1)}>+</button>
+        {counter}
+    </>
+}
